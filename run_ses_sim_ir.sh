48c338e858c88f23e66801b265ce31ed36833089
Chenghao Quan <65656088+QCH170308@users.noreply.github.com>
2023-05-22 17:49:53 +0900

    Update run_ses_sim_ir.sh

--- run_ses_sim_ir.sh
+++ run_ses_sim_ir.sh
@@ -13,20 +13,24 @@ dir=./rand_wght/rand_wght-${cbsize}-${nb}b
 #--------- Balanced Case -------------
 
 sp=spRw${rw}Double
+Ydir=$dir/$sp
 
-mkdir -p $dir/$sp
-mkdir -p $dir/lg
+mkdir -p $Ydir
+mkdir -p $Ydir/lg
   
-  dir=$dir rw=$rw ref=0 nb=$nb sp=$sp sig=0 alp=1 sa0=0 sa1=0 noise=0 gmin=$gmin gmax=$gmax ./SES/runmatall.sh 30
+  dir=$dir rw=$rw ref=0 nb=$nb Ydir=$Ydir sig=0 alp=1 sa0=0 sa1=0 noise=0 gmin=$gmin gmax=$gmax ./SES/runmatall.sh 30
   
 #-------------------------------------
 
 #--------- Unbalanced Case -----------
 
 #sp=spRw${rw}Ref$ref
-#mkdir -p $dir/$sp
+#Ydir=$dir/$sp
 
-  #dir=$dir rw=$rw ref=$ref nb=$nb sp=$sp sig=0 alp=1 sa0=0 sa1=0 noise=0 gmin=$gmin gmax=$gmax ./SES/runmatall.sh 30
+#mkdir -p $Ydir
+#mkdir -p $Ydir/lg
+
+  #dir=$dir rw=$rw ref=$ref nb=$nb Ydir=$Ydir sig=0 alp=1 sa0=0 sa1=0 noise=0 gmin=$gmin gmax=$gmax ./SES/runmatall.sh 30
   
 #-------------------------------------
 
